f471d9b5aa9eabbab46f199258fa9e3b76aa09b1
zhaoqi <1694743630@qq.com>
2022-09-14 21:12:02 +0800

    lodash

--- lodash/jayzhao68-lodash.js
+++ lodash/jayzhao68-lodash.js
@@ -19,4 +19,44 @@ var jayzhao68 = {
     }
     return newarray
   }
+
+
+  , compact:
+    function (array) {
+      var newarray = []
+      for (var i = 0; i < array.length; i++) {
+        if (array[i]) {
+          newarray.push(array[i])
+        }
+      }
+      return newarray
+    },
+
+  difference: function (array, [values]) {
+    var newarray = []
+    for (var i = 0; i < array.length; i++) {
+      if (!(array[i]) in values) {
+        newarray.push(array[i])
+      }
+    }
+    return newarray
+  },
+
+
+  drop: function (array, [n = 1]) {
+    var newarray = []
+    for (var i = n; i < array.length; i++) {
+      newarray.push(array[i])
+    }
+    return newarray
+  },
+
+  fill: function (array, value, [start = 0], [end = array.length]) {
+    for (var i = 0; i < array.length; i++) {
+      array.push(value)
+    }
+    return array
+  }
+
+
 }
